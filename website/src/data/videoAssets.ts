45e072d8f183621add6b04d3c88ba5562f28fa52
Philip Korniotes <p.korn79@gmail.com>
2026-08-22 12:32:47 -0500

    feat: add ScaleSafe video funnel

---
+++ website/src/data/videoAssets.ts
@@ -0,0 +1,11 @@
+export const explainerVideo = {
+  src: "https://assets.cdn.filesafe.space/dFHzk9pilPicHhxQyCNx/media/a4d01dec-f79b-44ba-a36d-6335b3480579.mp4",
+  poster: "https://assets.cdn.filesafe.space/dFHzk9pilPicHhxQyCNx/media/057eade8-b524-4a1b-8f7e-6dc4cf03e6db.jpg",
+  label: "ScaleSafe overview video",
+};
+
+export const walkthroughVideo = {
+  src: "https://assets.cdn.filesafe.space/dFHzk9pilPicHhxQyCNx/media/de68612d-f992-4089-aee1-bbb18a6d863d.mp4",
+  poster: "https://assets.cdn.filesafe.space/dFHzk9pilPicHhxQyCNx/media/ab462126-597c-415b-91be-b413337c2fba.jpg",
+  label: "ScaleSafe product walkthrough video",
+};
